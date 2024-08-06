00a9f6e0dd1e5e82ef22aeac6f55765f65117f08
MaldiniBeh <maldinibehan714@gmail.com>
2024-08-06 18:12:51 +0100

    start responsive design

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@ import Component from "./component.js";
     const data = GlobalData
     $("#collections").append(components.collectionsBtn(data.collectionsBtn));
     $("#menu").html(components.menu(data.menu));
+    $("#mobile-menu").prepend(components.menu(data.menu));
     $("#cards").html(components.card(data.categories));
     $("#wallet").html(components.wallet(data.wallets));
+    $("#menu-toggle").click((()=> $("#mobile-menu").toggleClass('hidden').fadeToggle( "slow", "linear" )));
 })(jQuery);
